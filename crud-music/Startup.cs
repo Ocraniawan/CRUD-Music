05a6ebc80a4e6800dadc2f75bdd13280e00cbfac
ocraniawan patattan <Ocraniawanpatattan@gmail.com>
2021-06-13 18:05:30 +0700

    Update Startup.cs
    - allow cors

--- crud-music/Startup.cs
+++ crud-music/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using crud_music.Models;
+using Microsoft.AspNetCore.Cors;
 
 namespace crud_music
 {
@@ -27,6 +28,13 @@ namespace crud_music
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+            });
             IServiceCollection serviceCollection = services.AddDbContext<dbMusicContext>(option =>
             option.UseSqlServer(Configuration.GetConnectionString("dbMusicContext")));
             services.AddControllers();
@@ -35,12 +43,14 @@ namespace crud_music
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
             app.UseHttpsRedirection();
+            app.UseCors("CorsPolicy");
 
             app.UseRouting();
 
